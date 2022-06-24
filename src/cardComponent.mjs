a304bb29afd6b9a23ab482999be9f5bdc426ebda
Joy Shaheb <istiaque.joy@northsouth.edu>
2022-06-25 02:33:50 +0600

    commented next prev buttons

--- src/cardComponent.mjs
+++ src/cardComponent.mjs
@@ -3,16 +3,16 @@ let buttons = document.getElementById("next-prev-buttons");
 
 export let cardComponent = (data) => {
   cardContainer.innerHTML = "";
-  buttons.innerHTML = "";
+  // buttons.innerHTML = "";
   console.log(data);
   let { hints, parsed, text, _links } = data;
 
   if (hints === undefined) return;
   else {
-    buttons.innerHTML = `
-      <button class="btn btn-dark">Prev</button>
-      <button class="btn btn-dark">Next</button>
-    `;
+    // buttons.innerHTML = `
+    //   <button class="btn btn-dark">Prev</button>
+    //   <button class="btn btn-dark">Next</button>
+    // `;
     cardContainer.innerHTML = hints
       ?.map((x) => {
         let { food, measures } = x;
